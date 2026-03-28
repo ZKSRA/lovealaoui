a163ac95aa5ba4b24829d35a78c67e6926d5624b
ZKSRA <109070250+ZKSRA@users.noreply.github.com>
2026-03-28 16:24:56 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://lovealaoui.com',
+  // CHANGE SITE LATER
+  site: 'https://89746726.microstore-6v7.pages.dev/#_',
   integrations: [sitemap()],
 });
