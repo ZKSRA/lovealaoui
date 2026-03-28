7a09aba7971a2d190338cd72017b6e6b21e66f82
ZKSRA <109070250+ZKSRA@users.noreply.github.com>
2026-03-28 18:24:41 +0000

    Remove mistakenly committed node_modules and add gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server',
   adapter: cloudflare(),
+  output: 'server',
+  site: 'https://lovealaoui.com',
   vite: {
     plugins: [tailwindcss()],
   },
-  // CHANGE SITE LATER
-  site: 'https://89746726.microstore-6v7.pages.dev/#_',
   integrations: [sitemap()],
 });
